9d536afb05b3bdbf96c51012a73fc8cd5c755054
Lain Musgrove <lain.proliant@gmail.com>
2020-11-22 19:40:33 -0800

    Lots of customization and lua custom rendering.

--- generate.py
+++ generate.py
@@ -17,17 +17,24 @@ def load_font_config():
 
 # -------------------------------------------------------------------
 def screen_count():
-    return int(subprocess.check_output('xrandr | grep " connected " | wc -l', shell=True).decode('utf-8'))
+    return (
+        int(
+            subprocess.check_output("xrandr --listmonitors | wc -l", shell=True).decode(
+                "utf-8"
+            )
+        )
+        - 1
+    )
 
 
 # -------------------------------------------------------------------
 def load_base16():
-    xdefaults_file = Path.home() / '.Xdefaults'
+    xdefaults_file = Path.home() / ".Xdefaults"
 
     base16 = {}
-    with open(xdefaults_file, 'r') as infile:
+    with open(xdefaults_file, "r") as infile:
         for line in infile.readlines():
-            match = re.match(r'#define (base.*) (#.*)$', line.strip())
+            match = re.match(r"#define (base.*) (#.*)$", line.strip())
             if match:
                 base16[match.group(1)] = match.group(2)
     return base16
@@ -45,8 +52,8 @@ def load_template(filename):
 
 # -------------------------------------------------------------------
 def main():
-    jinja_rc_files = Path.home().glob('.conky/rc/*.jinja')
-    existing_rc_files = Path.home().glob('.conky/rc/*.rc')
+    jinja_rc_files = Path.home().glob(".conky/rc/*.jinja")
+    existing_rc_files = Path.home().glob(".conky/rc/*.rc")
     font_config = load_font_config()
     for rc_file in existing_rc_files:
         rc_file.unlink()
@@ -54,10 +61,13 @@ def main():
     for jinja_rc_file in jinja_rc_files:
         for screen in range(0, screen_count()):
             template = load_template(jinja_rc_file)
-            rc_file = jinja_rc_file.with_suffix('.screen-%d.rc' % screen)
-            print('Writing %s' % rc_file)
-            with open(rc_file, 'w') as outfile:
-                print(template.render(screen=screen, **BASE16, **font_config), file=outfile)
+            rc_file = jinja_rc_file.with_suffix(".screen-%d.rc" % screen)
+            print("Writing %s" % rc_file)
+            with open(rc_file, "w") as outfile:
+                print(
+                    template.render(screen=screen, **BASE16, **font_config),
+                    file=outfile,
+                )
 
 
 # -------------------------------------------------------------------
